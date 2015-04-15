0aa5723bca8731494c15902d97c625b4fa46df92
Siriwat Kasamwattanarote <k_siriwatk@hotmail.com>
2015-04-15 15:34:12 +0900

    Optimized header for faster compilation.

--- ins_offline.h
+++ ins_offline.h
@@ -5,34 +5,7 @@
  *      Author: Siriwat Kasamwattanarote
  */
 #pragma once
-#include <sys/stat.h>   // file-directory existing
-#include <sys/types.h>  // file-directory
-#include <dirent.h>     // file-directory
-#include <cstdlib>
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-#include <sstream>
-#include <unordered_map>
-#include <bitset>
-#include <cmath>
 
-#include <flann/flann.hpp>
-#include "../lib/alphautils/alphautils.h"
-#include "../lib/alphautils/hdf5_io.h"
-#include "../lib/sifthesaff/SIFThesaff.h"
-#include "../lib/ins/ins_param.h"
-#include "../lib/ins/invert_index.h"
-#include "../lib/ins/bow.h"
-#include "../lib/ins/quantizer.h"
-
-#include "version.h"
-
-using namespace std;
-using namespace ::flann;
-using namespace alphautils;
-using namespace alphautils::hdf5io;
 using namespace ins;
 
 // private variable
@@ -40,13 +13,16 @@ using namespace ins;
 //-- Param
 ins_param run_param;
 //-- Dataset list
-vector<string> ImgParentPaths;
-vector<int> ImgListsPoolIds;                // To keep track of image_id to pool_id
-vector<size_t> ImgParentsIdx;
+deque<string> ParentPaths;
+vector<int> Img2PoolIdx;                    // To keep track of image_id to pool_id
+vector<size_t> Pool2ParentsIdx;
+vector< pair<size_t, size_t> > Pool2ImagesIdxRange;    // To map between pool id and image ids [start, end]
+vector<size_t> Img2ParentsIdx;
 vector<string> ImgLists;
 size_t total_features;
 vector<int> feature_count_per_pool;         // number of features per pool
 vector<int> feature_count_per_image;        // number of features per image
+vector<int> image_count_per_pool;           // number of images per pool
 //-- Quantized offset
 bool dataset_quantized_offset_ready;
 vector<size_t> dataset_quantized_offset;
@@ -58,14 +34,16 @@ void SaveDatasetList();
 void LoadDatasetList();
 void ProcessDataset();                      // Not necessary, this is for pre-processing image
 void ExtractDataset(bool save_feature);
-void PackFeature(bool by_block, size_t block_size);
+void PackFeature(size_t block_size);
 const int LOAD_DESC = 0, LOAD_KP = 1, LOAD_ALL = 2;
 void LoadFeature(size_t start_idx, size_t load_size, int load_mode, Matrix<float>& load_data);
-void SavePoolinfo(const string& out);
+void SavePoolinfo(const string& out, const bool print = true);
 void LoadPoolinfo(const string& in);
-string SamplingDatabase(int sample_size, int dimension);
+string SamplingDatabase(int sample_size);
+string SamplingOnTheFly();
 void Clustering(bool save_cluster, bool hdf5 = true);
 void SaveCluster(const string& out);
+void ClusteringCoarseLayer();
 void ImageFeaturesQuantization(bool save_quantized);
 void SaveQuantizedDataset(const vector<int>& quantized_counts, const vector<int*>& quantized_indices, const vector<float*>& quantized_dists, bool append);
 void LoadQuantizedDatasetOffset();
@@ -73,4 +51,14 @@ void LoadSpecificQuantizedDataset(vector<int>& quantized_counts, vector<int*>& q
 void ReleaseQuantizedOffset();
 void Bow(bool save_bow);
 void build_invert_index();
+
+void SiftCheck();
+void SiftPackRepair();
+void PoolCheck();
+void PoolRepair();
+void QuantizedCorrectnessCheck();
+void BowCorrectnessCheck();
+void InvDefCorrectnessCheck();
+void PoolingTester();
+void ExportImgList();
 //;)
